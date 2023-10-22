a4a5c288e38a99b21d1db5bd96187aeafe7fe5c4
Abdulrahman-salem <abdalrhman2000salem@gmail.com>
2023-10-22 22:05:32 +0200

    1) importing Jura font 2) creating scss mixins: jura-bold, jura-medium, jura

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,17 +1,13 @@
 import "./index.scss";
-import NavBar from "../../components/NavBar/NavBar";
-import Description from "../../components/Description_home/Description_home";
+import NavBar from "../../components/NavBar";
+import Description from "../../components/Description_home";
 
 function Home() {
     return (
-      <div>
-        {/* <p className="red">Home page</p>
-        <p>env: {process.env.REACT_APP_SAME_NUMBER}</p> */}
         <>
-          <NavBar />
-          <Description />
+            <NavBar />
+            <Description />
         </>
-      </div>
     );
 }
 
