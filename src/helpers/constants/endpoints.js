01000d257790bc2ccfec5c1d1510c555496b9a8d
Abdulrahman-salem <109165947+Abdulrahman-salem@users.noreply.github.com>
2023-11-02 11:38:25 +0100

    Update endpoints.js

--- src/helpers/constants/endpoints.js
+++ src/helpers/constants/endpoints.js
@@ -1,5 +1,5 @@
 export const URL_STUDENTS = "http://localhost:3001/students";
-export const QUERY_TO_FETCH_NEXT_PAGE_STUDENTs = `offset=`;
+export const QUERY_TO_FETCH_NEXT_PAGE_STUDENTS = `offset=`;
 export const QUERY_FILTER_STUDENTS_BY_A_TO_Z = `filterBy=a-z`;
 export const QUERY_FILTER_STUDENTS_BY_Z_TO_A = `filterBy=z-a`;
 export const URL_FILTER_STUDENT_BY_NAME = `${URL_STUDENTS}?studentFullName=`;
