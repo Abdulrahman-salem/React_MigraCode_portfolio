22609e961fbd589e9bb75763ac8b67436e077667
Abdulrahman-salem <abdalrhman2000salem@gmail.com>
2023-11-09 02:37:17 +0100

    1) Implementing React Redux for projects instead of using state. 2) Creating Redux storage for students.

--- src/App.js
+++ src/App.js
@@ -5,17 +5,66 @@ import Projects from "./pages/Projects";
 import Project from "./pages/project";
 import Students from "./pages/Students";
 
-
+// import React, { useEffect } from "react";
+// import { getData } from "./adapters/fetch";
+// import { URL_PROJECTS } from "./helpers/constants/endpoints";
+// import { useDispatch, useSelector } from "react-redux";
+// import {
+//     firstFetchedProjects,
+//     fetchingProjects,
+//     resetProjectsState,
+// } from "./redux/projects";
 
 function App() {
-    return (
-      <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/projects" element={<Projects/>} />
-          <Route path="/project" element={<Project/>} />
-          <Route path="/students" element={<Students/>} />
+    // // to read redux projects state
+    // const { projectsState } = useSelector((store) => store);
 
-      </Routes>
+    // // to set redux state
+    // const dispatch = useDispatch();
+
+    // useEffect(() => {
+    //     // fetchingProjects is before starting fetching data because of loader
+    //     dispatch(fetchingProjects({}));
+    //     if (projectsState.projects.length === 0) {
+    //         async function fetchData(fetchRequirement) {
+    //             const { url } = fetchRequirement;
+    //             try {
+    //                 const data = await getData(url);
+    //                 if (data) {
+    //                     // console.log(data);
+    //                     data.items.forEach((project) => {
+    //                         if (project.project_image_link.length === 0) {
+    //                             project.project_image_link =
+    //                                 require("./assets/images/default_project_img.svg").default;
+    //                         }
+    //                     });
+    //                     // to set first fetching
+    //                     dispatch(
+    //                         firstFetchedProjects({
+    //                             projects: [...data.items],
+    //                             nextPage: data.nextpage
+    //                                 ? data.nextpage.toString()
+    //                                 : "",
+    //                         })
+    //                     );
+    //                 }
+    //             } catch (error) {
+    //                 console.log(error.message);
+    //             }
+    //         }
+    //         fetchData({ url: URL_PROJECTS });
+    //         // to reset projects State before going to Projects page
+    //         dispatch(resetProjectsState());
+    //     }
+    // }, []);
+
+    return (
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/project" element={<Project />} />
+            <Route path="/students" element={<Students />} />
+        </Routes>
     );
 }
 
