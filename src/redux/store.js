a033ca00826ccfeb720dfea7171e16fd2a82aa2b
Abdulrahman-salem <abdalrhman2000salem@gmail.com>
2023-11-22 04:33:57 +0100

    Revert "Revert "Revert "0"""
    This reverts commit 0c1251ce39335df8538ba0fdfd798de03869c85f.

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import projectsReducer from "./projects";
 import studentsReducer from "./students";
+import logInReducer from "./logIn";
 
 const store = configureStore({
     reducer: {
         projectsState : projectsReducer,
         studentsState : studentsReducer,
+        logInState : logInReducer,
     },
 });
 
