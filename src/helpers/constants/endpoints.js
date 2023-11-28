e37e880cc60c2830514f08fc0c3a28cba5d20cc0
Abdulrahman-salem <abdalrhman2000salem@gmail.com>
2023-11-28 21:49:31 +0100

    fix endpoints.js

--- src/helpers/constants/endpoints.js
+++ src/helpers/constants/endpoints.js
@@ -3,11 +3,11 @@ export const URL_LOGIN = "https://node-migra-code-portfolio-umber.vercel.app/log
 
 //portfolio
 export const URL_PORTFOLIO =
-  "http://localhost:3001/student";
+  "https://node-migra-code-portfolio-umber.vercel.app/student";
 
 // students
 export const URL_STUDENTS =
-  "http://localhost:3001/students";
+  "https://node-migra-code-portfolio-umber.vercel.app/students";
 export const QUERY_TO_FETCH_NEXT_PAGE_STUDENTS = `offset=`;
 export const QUERY_FILTER_STUDENTS = `filterBy`;
 export const QUERY_FILTER_STUDENTS_BY_A_TO_Z = `a-z`;
@@ -19,7 +19,7 @@ export const NUMBER_OF_PROJECTS_DISPLAYED = 2;
 
 // projects
 export const URL_PROJECTS =
-  "http://localhost:3001/projects";
+  "https://node-migra-code-portfolio-umber.vercel.app/projects";
 export const QUERY_TO_FETCH_NEXT_PAGE_PROJECTS = `nextpage=`;
 export const QUERY_FILTER_PROJECTS = `orderby`;
 export const QUERY_FILTER_PROJECTS_BY_A_TO_Z = `a-z`;
@@ -27,7 +27,7 @@ export const QUERY_FILTER_PROJECTS_BY_Z_TO_A = `z-a`;
 export const QUERY_FILTER_PROJECTS_BY_OLDEST_TO_NEWEST = `date-a-z`;
 export const QUERY_FILTER_PROJECTS_BY_NEWEST_TO_OLDEST = `date-z-a`;
 export const URL_NEW_PROJECT =
-  "http://localhost:3001/projects";
+  "https://node-migra-code-portfolio-umber.vercel.app/projects";
 
 // Official MigraCode website
 export const LINK_OFFICIAL_MIGRACODE_WEBSITE =
